3332b47f95d034338a4a2b8e8e892bed4b23f8aa
AlexeyBarbosa <alexbarbosantos@gmail.com>
2022-10-17 10:17:21 -0500

    1 Clase

---
+++ src/Mensaje.js
@@ -0,0 +1,32 @@
+export function Mensaje() {
+    return "Desarrollo de software!";
+}
+
+export function titulo() {
+    return <h1>Hola mundo!</h1>;
+}
+
+export function Asistentes(nombre, cant) {
+    return <h1>Hay {cant} {nombre} </h1>;
+}
+
+export const printEvento = (evento) => {
+    return <div>
+        <h1>{evento.equipo1.nombre}</h1>
+        <p> {evento.marcador1} </p>
+
+        <h1>{evento.equipo2.nombre}</h1>
+        <p> {evento.marcador2} </p>
+        <p>{ganador(evento)}</p>
+    </div>;
+}
+
+const ganador = (evento) => {
+    if (evento.marcador1 === evento.marcador2) {
+        return "empate";
+    } else if (evento.marcador1 > evento.marcador2) {
+        return "ganó " + evento.equipo1.nombre;
+    } else {
+        return "ganó " + evento.equipo2.nombre;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom/client';
+import { Mensaje, titulo, Asistentes, printEvento } from './Mensaje';
+import Header from './Header';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDom.createRoot(document.getElementById('root'));
+let eventoPrueba = {
+    "equipo1": {
+        "nombre": "Selcción Colombiana de Fútbol"
+    },
+    "equipo2": {
+        "nombre": "Selección Nacional de Tennis"
+    },
+    "marcador1": 1,
+    "marcador2": 5
+};
+root.render(<>
+    <Header />
+    <h1>{titulo()}</h1>
+    <p> <Mensaje /> </p>
+    <div> {Asistentes("jugadores", 40)} </div>
+    {printEvento(eventoPrueba)}
+</>);
